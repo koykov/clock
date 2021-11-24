aff20e98adc1150309306a849b00c09e9e023cd3
Konstantin Voykov <koykov0@gmail.com>
2021-11-24 17:57:01 +0200

    Add readme; code comment issues.

--- clock.go
+++ clock.go
@@ -11,22 +11,30 @@ const (
 	StatusActive
 )
 
+// Clock is a fast replacement of base methods of time package.
 type Clock struct {
+	// Clock precision.
+	// Settings this param too small (less than microseconds) or too big (great than second) is counterproductive.
 	Precision time.Duration
+
 	status    int32
 	sec, nsec int64
-	cancel    context.CancelFunc
+
+	cancel context.CancelFunc
 }
 
+// NewClock makes new clock with default precision (1 millisecond).
 func NewClock() *Clock {
 	return NewClockWP(time.Millisecond)
 }
 
+// NewClockWP makes new clock with given precision.
 func NewClockWP(precision time.Duration) *Clock {
 	c := &Clock{Precision: precision}
 	return c
 }
 
+// Start initializes and starts the clock.
 func (c *Clock) Start() {
 	if atomic.LoadInt32(&c.status) == StatusActive {
 		return
@@ -59,6 +67,7 @@ func (c *Clock) Stop() {
 	}
 }
 
+// Now returns current time.
 func (c *Clock) Now() time.Time {
 	return time.Unix(atomic.LoadInt64(&c.sec), atomic.LoadInt64(&c.nsec))
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Clock
+
+Fast analogue of time package methods.
+
+## Benchmarks
+```
+BenchmarkClock/clock.Now()-8         	1000000000          0.8397 ns/op
+BenchmarkClock/time.Now()-8          	20795817	        57.93 ns/op
+```

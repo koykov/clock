b78046e92560ad15c550e48770e15c27bc67a206
Konstantin Voykov <koykov0@gmail.com>
2021-11-24 17:46:54 +0200

    Cover clock with tests.

--- clock_test.go
+++ clock_test.go
@@ -5,6 +5,32 @@ import (
 	"time"
 )
 
+func TestClock(t *testing.T) {
+	t.Run("diff time.Nov() - clock.Now()", func(t *testing.T) {
+		const (
+			prec  = time.Millisecond
+			allow = prec + prec/2
+		)
+		var (
+			diff time.Duration
+			fail bool
+		)
+		c := NewClockWP(prec)
+		c.Start()
+		for i := 0; i < 500; i++ {
+			if diff = time.Now().Sub(c.Now()); diff > allow {
+				fail = true
+				break
+			}
+			time.Sleep(time.Millisecond)
+		}
+		c.Stop()
+		if fail {
+			t.Errorf("diff is too big: %s, max %s", diff, allow)
+		}
+	})
+}
+
 func BenchmarkClock(b *testing.B) {
 	b.Run("clock.Now()", func(b *testing.B) {
 		c := NewClock()
